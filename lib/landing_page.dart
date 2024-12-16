871734d90b7222ed944e0e9949e891195af4f56a
YonsakSaengsuk <96975135+ToKuMoNeo@users.noreply.github.com>
2024-12-16 19:52:57 +0700

    Landing_page

---
+++ lib/landing_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Todolist"),
+          SizedBox(height: 10),
+          Column(
+            children: [
+              
+          ])
+        ],
+      ),
+    );
+  }
+}
